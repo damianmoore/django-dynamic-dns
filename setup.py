ae1acba9feebf9b98f3c0b51beb3ad884abd4354
Damian Moore <damian@epixstudios.co.uk>
2014-03-30 22:38:22 +0100

    Version number bump

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name = "dynamicdns",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Damian Moore",
     author_email = "django-dynamic-dns@epixstudios.co.uk",
     description = ("Machines on dynamic IPs can call this service at regular intervals (e.g. via cron) and when the server notices an address change it makes the relevant API call to update the DNS provider. Alternatively a simple DNS server is included which can be run instead of using an external DNS provider."),
     license = "BSD",
     keywords = "django dynamic dns server ip domain service",
     url = "https://github.com/damianmoore/django-dynamic-dns",
-    download_url = 'https://github.com/damianmoore/django-dynamic-dns/tarball/0.1.0',
+    download_url = 'https://github.com/damianmoore/django-dynamic-dns/tarball/0.1.1',
     packages = [
         'dynamicdns',
     ],
