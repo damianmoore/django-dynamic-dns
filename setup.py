820318ff1569c0436ac52e4d6d0167ed0a2a9e6f
Damian Moore <damian@epixstudios.co.uk>
2014-04-10 09:21:45 +0100

    Changed download URL as PyPI seems to have problem with it

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name = "django-dynamic-dns",
-    version = "0.1.2",
+    version = "0.1.3",
     author = "Damian Moore",
     author_email = "django-dynamic-dns@epixstudios.co.uk",
     description = ("Machines on dynamic IPs can call this service at regular intervals (e.g. via cron) and when the server notices an address change it makes the relevant API call to update the DNS provider. Alternatively a simple DNS server is included which can be run instead of using an external DNS provider."),
     license = "BSD",
     keywords = "django dynamic dns server ip domain service",
     url = "https://github.com/damianmoore/django-dynamic-dns",
-    download_url = 'https://github.com/damianmoore/django-dynamic-dns/tarball/0.1.2',
+    download_url = 'https://github.com/damianmoore/django-dynamic-dns/archive/0.1.3.zip',
     packages = [
         'dynamicdns',
     ],
